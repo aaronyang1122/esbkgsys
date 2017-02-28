ba6cc4a1f15e246262797d310ccf51edbb463f4a
babyang <aaronyang1122@gmail.com>
2017-02-28 16:49:26 +0800

    modified static path

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 
 // static service
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
+    res.header("X-Powered-By",' 3.2.1');
     // res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
