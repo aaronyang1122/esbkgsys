3872f55318c29f3cd87b10b7ed9132ccdf712202
babyang <aaronyang1122@gmail.com>
2017-03-01 17:16:01 +0800

    format date for all api

--- controllers/news.js
+++ controllers/news.js
@@ -6,9 +6,14 @@ var News = require('../service').News;
 exports.list = function (req, res, next) {
     News.list(function (err, list) {
         if (err) return next(err);
+        list.forEach(function (e) {
+            e.createtime = new Date(e.createtime).Format("yyyy-MM-dd hh:mm:ss");
+            e.updatetime = new Date(e.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+            return e;
+        });
         res.send(list);
     })
-}
+};
 
 exports.add = function (req, res, next) {
     var checkkeys = true;
@@ -23,14 +28,18 @@ exports.add = function (req, res, next) {
     } else {
         res.status(500).send('bad params request')
     }
-}
+};
 
 exports.detail = function (req, res, next) {
     News.detail(req.query.id, function (err, detail) {
         if (err) return next(err);
+        if (detail) {
+            detail.createtime = new Date(detail.createtime).Format("yyyy-MM-dd hh:mm:ss");
+            detail.updatetime = new Date(detail.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+        }
         res.send(detail===null?{}:detail);
     })
-}
+};
 
 exports.delete = function (req, res, next) {
     News.delete(req.body.id, function (err) {
@@ -38,11 +47,13 @@ exports.delete = function (req, res, next) {
         if (err) return next(err);
         res.send('delete successful');
     })
-}
+};
 
 exports.update = function (req, res, next) {
     News.update(req.query.id, req.body, function (err, item) {
         if (err) return next(err);
+        item.updatetime = new Date(item.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+        item.createtime = new Date(item.createtime).Format("yyyy-MM-dd hh:mm:ss");
         res.send(item);
     })
-}
+};
--- controllers/product.js
+++ controllers/product.js
@@ -7,9 +7,14 @@ var Product = require('../service').Product;
 exports.list = function (req, res, next) {
     Product.list(function (err, list) {
         if (err) return next(err);
+        list.forEach(function (e) {
+            e.createtime = new Date(e.createtime).Format("yyyy-MM-dd hh:mm:ss");
+            e.updatetime = new Date(e.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+            return e;
+        });
         res.send(list);
     })
-}
+};
 
 exports.add = function (req, res, next) {
     var checkkeys = true;
@@ -24,14 +29,18 @@ exports.add = function (req, res, next) {
     } else {
         res.status(500).send('bad params request')
     }
-}
+};
 
 exports.detail = function (req, res, next) {
     Product.detail(req.query.id, function (err, detail) {
         if (err) return next(err);
+        if (detail) {
+            detail.createtime = new Date(detail.createtime).Format("yyyy-MM-dd hh:mm:ss");
+            detail.updatetime = new Date(detail.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+        }
         res.send(detail===null?{}:detail);
     })
-}
+};
 
 exports.delete = function (req, res, next) {
     Product.delete(req.body.id, function (err) {
@@ -39,11 +48,13 @@ exports.delete = function (req, res, next) {
         if (err) return next(err);
         res.send('delete successful');
     })
-}
+};
 
 exports.update = function (req, res, next) {
     Product.update(req.query.id, req.body, function (err, item) {
         if (err) return next(err);
+        item.updatetime = new Date(item.updatetime).Format("yyyy-MM-dd hh:mm:ss");
+        item.createtime = new Date(item.createtime).Format("yyyy-MM-dd hh:mm:ss");
         res.send(item);
     })
-}
+};
