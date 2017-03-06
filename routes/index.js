e5b085708db7f2b43b50ebfaae790c697f9e7f8e
babyang <aaronyang1122@gmail.com>
2017-03-06 12:58:25 +0800

    update context about index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', body: 'espacetime api background system' });
+  res.render('index', { title: 'Website Building', body: 'The website is building, please come back later' });
 });
 
 module.exports = router;
